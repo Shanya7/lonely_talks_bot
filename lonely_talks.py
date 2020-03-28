805eee814f9781fe7e05d43be69cf96b32461902
annas <anna.shepel7@outlook.com>
2020-03-28 17:00:32 +0200

    updated talks part

--- lonely_talks.py
+++ lonely_talks.py
@@ -1,9 +1,34 @@
 import telebot
 
 bot = telebot.TeleBot('1104778803:AAHv9g6lHKJ6-T7UJL54KHEcYcm5AXSFASQ')
+# Start "Welcome" message
+#@bot.message_handler(commands=['start'])
+#def start_message(message):
+#    bot.send_message(message.chat.id, "О, {0.first_name}! \n Приветики-пистолетики, поговорим? ".format(message.from_user))
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+# Talks
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, "О, {0.first_name}! \nПриветики-пистолетики, поговорим? ".format(message.from_user))
+    elif message.text.lower() == 'что ты умеешь?' or message.text.lower() =='уу':
+        bot.send_message(message.chat.id, 'нн')
+    elif message.text.lower() == 'мне скучно':
+        bot.send_message(message.chat.id, 'У меня есть шутка')
+    elif message.text.lower() == 'какая':
+        bot.send_message(message.chat.id, 'Говорят, после курсов по раскрытию паранормальных способностей, лучшие выпускники уходят в ясновидящие, а худшие в гидрометцентр')
+    elif message.text.lower() == 'не смешно':
+        bot.send_message(message.chat.id, 'ой, всё!')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'я тебя люблю':
+        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+    elif message.text.lower() == 'что ты умеешь?':
+        bot.send_sticker(message.chat.id, 'Шутить')
+    else:
+        bot.send_message(message.chat.id, 'эм, не понял')
 
+
+#def joke (message:)
+#def worldwide_search (message):
 bot.polling()
